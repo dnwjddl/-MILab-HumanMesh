ecb4ff7b0e31ead153d641774c982671c564d96f
dnwjddl <woojung980305@gmail.com>
2021-07-06 18:13:49 +0900

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class HMRTrainer(object):
+  def __init__(self):
+  def train(self):
+    def save_model(result):
+
+      
+def main():
+  trainer = HMRTrainer()
+  trainer.train()
+  
+if __name__ == '__main__':
+  main()
+      
