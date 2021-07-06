6eb59b609c8a3eba33fe0eb6b9547d038be16ed3
dnwjddl <woojung980305@gmail.com>
2021-07-06 18:19:50 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,8 +1,25 @@
 import torch
 import torch.nn as nn
 
+import config
+from config import args
+
+from model import HMRNetBase
+
 class HMRTrainer(object):
   def __init__(self):
+    
+    self._build_model()
+    self._create_data_loader()
+  
+  def _create_data_loader(self):
+    '''dataloader'''
+    
+  def _build_model(self):
+    print('start building model')
+    generator = HMRNetBase()
+    model_path = config.pre_trained_model['generator']
+    
   def train(self):
     def save_model(result):
 
